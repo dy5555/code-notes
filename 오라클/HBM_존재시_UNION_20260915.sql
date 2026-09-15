e2d675d6517d688c4102e245c5c6dc3f77862855
59 <47468864+dy5555@users.noreply.github.com>
2026-09-15 10:03:11 +0900

    HBM 코드만 대상으로 LIKE EXISTS 비교하도록 수정

--- 오라클/HBM_존재시_UNION_20260915.sql
+++ 오라클/HBM_존재시_UNION_20260915.sql
@@ -1,19 +1,14 @@
--- A 조회 결과에 CODE = 'HBM'이 존재할 경우
--- B에서 CODE가 'HBM%'인 데이터를 추가로 UNION
+-- A에 여러 CODE가 있어도 HBM 관련 CODE만 대상으로 비교
+-- 예: A = HBM3, DDR5 / B = HBM3E, HBM4E, DDR5E
+-- 결과: HBM3E
 
-SELECT A.CODE
-     , A.NAME
-  FROM TABLE_A A
-
-UNION
-
-SELECT B.CODE
-     , B.NAME
-  FROM TABLE_B B
+SELECT B.*
+  FROM B
  WHERE B.CODE LIKE 'HBM%'
    AND EXISTS (
        SELECT 1
-         FROM TABLE_A C
-        WHERE C.CODE = 'HBM'
+         FROM A
+        WHERE A.CODE LIKE 'HBM%'
+          AND B.CODE LIKE A.CODE || '%'
        )
 ;
